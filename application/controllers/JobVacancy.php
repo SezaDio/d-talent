ed20a1e9048c0571e850f5d2f29829dc84fb5bf2
abdulazieskurniawan <abdulazies.k@gmail.com>
2018-07-20 09:17:48 +0700

    job list
    minus id company to name company
    id city to name city
    id provice to name province

--- application/controllers/JobVacancy.php
+++ application/controllers/JobVacancy.php
@@ -47,4 +47,49 @@ class JobVacancy extends CI_Controller
 		$this->load->view('content_front_end/job_list');
 		$this->load->view('skin/front_end/footer_company_page');
 	}
+	
+	function search_job(){
+		header('Access-Control-Allow-Origin: *');
+        header('Content-type: text/xml');
+		$description=$_POST['description'];
+		$category=$_POST['category'];
+		$type=$_POST['type'];
+		
+		
+		$data = '';
+		$data .= 'job_category LIKE "%'.$this->db->escape_like_str($category).'%"';
+		$data .= 'AND job_type LIKE "%'.$this->db->escape_like_str($type).'%"';
+		$data .= 'AND job_description LIKE "%'.$this->db->escape_like_str($description).'%"';
+		$get_job=$this->db->where($data)->get('job_vacancy');
+		$this->load->helper('xml');
+		$xml_out = '<jobs>';
+		if ($get_job->num_rows()>0) {
+            foreach ($get_job->result() as $row_job) {
+				/*$tanggal = date("j", strtotime($row_event->tgl_mulai));
+	            $bulan = date("M", strtotime($row_event->tgl_mulai));
+				$deskripsi=strip_tags($row_event->deskripsi_coming);
+				$deskripsi=substr($deskripsi,0,400);
+					*/			
+                $xml_out .= '<job ';
+                $xml_out .= 'id_job="' . xml_convert($row_job->id_job) . '" ';
+                $xml_out .= 'id_company="' . xml_convert($row_job->id_company) . '" ';
+                $xml_out .= 'job_title="' . xml_convert($row_job->job_title) . '" ';
+                $xml_out .= 'job_type="' . xml_convert($row_job->job_type) . '" ';
+                $xml_out .= 'job_category="' . xml_convert($row_job->job_category) . '" ';
+                $xml_out .= 'job_role="' . xml_convert($row_job->job_role) . '" ';
+                $xml_out .= 'job_province_location_id="' . xml_convert($row_job->job_province_location_id) . '" ';
+                $xml_out .= 'job_city_location_id="' . xml_convert($row_job->job_city_location_id) . '" ';
+                $xml_out .= 'job_date_start="' . xml_convert($row_job->job_date_start) . '" ';
+                $xml_out .= 'job_date_end="' . xml_convert($row_job->job_date_end) . '" ';
+                $xml_out .= 'job_description="' . xml_convert($row_job->job_description) . '" ';
+                $xml_out .= 'job_required_skill="' . xml_convert($row_job->job_required_skill) . '" ';
+                $xml_out .= 'publish_date="' . xml_convert($row_job->publish_date) . '" ';
+                
+                $xml_out .= '/>';
+            }
+        }
+		$xml_out .= '</jobs>';
+		
+        echo $xml_out;
+	}
 }
--- application/views/content_front_end/job_list.php
+++ application/views/content_front_end/job_list.php
@@ -8,8 +8,8 @@
 				<!--Jobs Caategory section-->
 				<div class="col-md-12">
 					<div class="row">
-						<div class="col-md-4" style="text-align: center; padding: 5px;">
-						<input type="text" id="valCategory2" value="">
+						<div class="col-md-3" style="text-align: center; padding: 5px;">
+						<input type="text" id="valCategory2" value="" style="display:none">
 							<div class="dropdown">
 								<button class="button button1 dropdown-toggle" type="button" id="Category" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 									<span id="valCategory">Job Category</span>
@@ -21,43 +21,44 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4" style="text-align: center; padding: 5px;">
+						<div class="col-md-3" style="text-align: center; padding: 5px;">
+						<input type="text" id="valType2" value="" style="display:none">
 							<div class="dropdown">
-								<button class="button button1 dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Pendidikan
+								<button class="button button1 dropdown-toggle" type="button" id="Type" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<span id="valType">Job Type</span>
 								</button>
 								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-									<p class="dropdown-item">Magister (S2)</p>
-									<p class="dropdown-item">Sarjana (S1)</p>
-									<p class="dropdown-item">Diploma (D3)</p>
+									<p class="dropdown-item" onclick="type('Full Time')">Full Time</p>
+									<p class="dropdown-item" onclick="type('Part Time')">Part Time</p>
+									<p class="dropdown-item" onclick="type('Internship')">Internship</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4" style="text-align: center; padding: 5px;">
+						<div class="col-md-3" style="text-align: center; padding: 5px;">
+						<input type="text" id="valBidIndustri2" value="" style="display:none">
 							<div class="dropdown">
-								<button class="button button1 dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Bidang Industri
+								<button class="button button1 dropdown-toggle" type="button" id="BidIndustri" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<span id="valBidIndustri">Bidang Industri</span>
 								</button>
 								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-									<p class="dropdown-item">Semua Industri</p>
-									<p class="dropdown-item">Arsitek</p>
-									<p class="dropdown-item">Industri Kimia</p>
-									<p class="dropdown-item">Elektronik</p>
+									<p class="dropdown-item" onclick="bidindustri('all')">Semua Industri</p>
+									<p class="dropdown-item" onclick="bidindustri('Arsitek')">Arsitek</p>
+									<p class="dropdown-item" onclick="bidindustri('Industri Kimia')">Industri Kimia</p>
+									<p class="dropdown-item" onclick="bidindustri('Elektronik')">Elektronik</p>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-2">
-							<button class="button button1 dropdown-toggle" type="button" style="background-color: #0066ff;border:#0066ff;">
-									Search
-							</button>
+						<div class="col-md-3" style="text-align: center; padding: 5px;">
+							<div class="input-group">
+								<input style=" border-color: black; background-color: white; color: black;" type="text" class="form-control" name="text" placeholder="Search Job . . ." required id="valDescription">
+								<span class="input-group-addon" style="background-color: black; color: white;" onclick="search_job()"><i class="fa fa-search"></i></span>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<hr style="border: solid 1px lightgray;">
-			<div class="col-md-12">
+			<div class="col-md-12" id="job_all">
 				<!--Show Jobs list-->
 				<div class="row">
 					<div class="col-md-6" style="border-left: solid 4px black; margin-bottom: 15px; padding-left: 0">
@@ -100,6 +101,8 @@
 					</div>
 				</div>
 			</div>
+			<div class="col-md-12" id="job_search" style="display:none">
+			</div>
 			<br>
 		</div>
 	</div>
@@ -108,8 +111,77 @@
 </div>
 <br>
 <script>
+	function parseXml(str) {
+		  if (window.ActiveXObject) {
+			var doc = new ActiveXObject('Microsoft.XMLDOM');
+			doc.loadXML(str);
+			return doc;
+		  } else if (window.DOMParser) {
+			return (new DOMParser).parseFromString(str, 'text/xml');
+		  }
+	  }
+	  
+	  
 	function category(x) {
 	  document.getElementById("valCategory").innerHTML = ""+x+"";
 	  document.getElementById("valCategory2").value = ""+x+"";
+	}
+	function type(y) {
+	  document.getElementById("valType").innerHTML = ""+y+"";
+	  document.getElementById("valType2").value = ""+y+"";
+	}
+	function bidindustri(z) {
+	  document.getElementById("valBidIndustri").innerHTML = ""+z+"";
+	  document.getElementById("valBidIndustri2").value = ""+z+"";
+	}
+	
+	//fungsi search
+	function search_job(){
+		var description = document.getElementById("valDescription").value;
+		var category = document.getElementById("valCategory2").value;
+		var type = document.getElementById("valType2").value;		
+		var bidindustri = document.getElementById("valBidIndustri2").value;		
+		alert(category);
+		alert(type);
+		alert(bidindustri);
+		document.getElementById("job_all").style.display = "none";
+		document.getElementById("job_search").style.display = "block";
+		$.post('<?php echo site_url('JobVacancy/search_job/'); ?>', {description:description,category:category,type:type}, function(dataJob){
+			var xml = parseXml(dataJob);
+			var getJob = xml.documentElement.getElementsByTagName("job");
+			var div_job_search = '';
+			div_job_search += '<div class="row">';
+			for (var i = 0; i < getJob.length; i++) {
+				var id_job = getJob[i].getAttribute("id_job");
+				var id_company = getJob[i].getAttribute("id_company");
+				var job_title = getJob[i].getAttribute("job_title");
+				var job_category = getJob[i].getAttribute("job_category");
+				var job_city_location_id = getJob[i].getAttribute("job_city_location_id");
+				var job_province_location_id = getJob[i].getAttribute("job_province_location_id");
+				
+				div_job_search += '<div class="col-md-6" style="border-left: solid 4px black; margin-bottom: 15px; padding-left: 0">';
+					div_job_search += '<div class="col-md-12" style="background-color: white;">';
+						div_job_search += '<div style="padding-top: 10px;  padding-bottom: 10px;">';
+							div_job_search += '<strong style="padding-top: 5px;font-size: 1.3em;">';
+								div_job_search += ''+job_title+'';
+							div_job_search += '</strong>';
+						div_job_search += '</div>';
+						div_job_search += '<small style="font-size: 1em;"><b>'+id_company+'</b></small>';
+						div_job_search += '<p style="font-size: 1em;">'+job_city_location_id+', '+job_province_location_id+'</p>';
+						div_job_search += '<hr style="border: solid 1px lightgray;">';
+						div_job_search += '<div style="padding-bottom: 10px;">';
+							div_job_search += '<small style="font-size: 1em;">Category : </small>';
+							div_job_search += '<span class="badge badge-dark">'+job_category+'</span>';
+						div_job_search += '</div>';
+					div_job_search += '</div>';
+				div_job_search += '</div>';
+				
+				
+				
+					
+			}
+			div_job_search += '</div>';
+			document.getElementById("job_search").innerHTML = div_job_search;
+		},"text");
 	}
 </script>
