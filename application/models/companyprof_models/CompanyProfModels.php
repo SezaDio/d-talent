430ee28cce7a2c3aae33c30681ebfe316e40309d
C-za <sefirman12@gmail.com>
2018-07-31 17:04:21 +0700

    keetinggalan

--- application/models/companyprof_models/CompanyProfModels.php
+++ application/models/companyprof_models/CompanyProfModels.php
@@ -41,7 +41,7 @@
 		//Mengambil data slider (Kecil)
 		function get_slider_small()
 		{
-			$query = $this->db->query("SELECT * FROM `slider` WHERE tipe=2 ORDER BY id_slider DESC");
+			$query = $this->db->query("SELECT * FROM `slider` WHERE type=2 ORDER BY id_slider DESC");
 		
 			$indeks = 0;
 			$result = array();
